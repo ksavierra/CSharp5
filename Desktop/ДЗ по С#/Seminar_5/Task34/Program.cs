ad1d4640d5e822610cd9b6bb8fff4ebf493e99c9
Kseniya <ksavierra@rambler.ru>
2022-07-21 14:40:30 +0500

    Решение задачи 34

---
+++ Desktop/ДЗ по С#/Seminar_5/Task34/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+static void Main(string[] args)
+        {
+            int[] array = new int[4];
+            Random RandomArray = new Random();
+ 
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = RandomArray.Next(100, 1000);
+                Console.Write("{0} ", array[i]);
+            }
+                int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+            if (array[i]%2 == 0)
+            {
+                count += 1;
+            }
+            
+            }
+            Console.Write("-> {0}", count);
+         }
+Main(args);
+       
