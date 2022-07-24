b229bc76f17149c175c9d87b2e4a61387f4cac0c
Kseniya <ksavierra@rambler.ru>
2022-07-24 12:15:58 +0500

    Решение задачи 36

---
+++ Desktop/ДЗ по С#/Seminar_5/Task36/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях/
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Введите количество элементов массива: ");
+
+    int l = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[l];
+    float sum = 0;
+ 
+    Random random = new Random();
+
+    for (int i = 0; i < l; i++)
+        {
+            array[i] = random.Next(0, 1000);
+            Console.Write(array[i] + " ");
+        }
+
+    for (int i = 0; i < l; i += 2) 
+        {
+            sum = sum + array[i];            
+        }
+ 
+            Console.Write("{0}" , "-> ");
+            Console.Write(sum);
+            Console.ReadKey();
+} 
+
+Main(args);
+
+
+
+
